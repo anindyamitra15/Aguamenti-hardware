294439812134079824bafebf63089a9cca861d7e
Anindya Mitra <anindya15l12@gmail.com>
2022-10-16 23:03:30 +0530

    init

---
+++ firmwares/pump-firmware/src/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * pump firmware main file
+ * connects the pump to Aguamenti server
+ * sync pump state thru socket
+ * authorize itself with HTTP
+ */
+
+#include <Arduino.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
